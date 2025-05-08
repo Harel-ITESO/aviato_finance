10b31a94e547ac6924d1f77fac85ea2c9e1e1eb4
César Sainz <cesar@5CG22937H5.ds.iteso.mx>
2025-05-08 10:22:31 -0600

    PathFix

--- lib/utils/file_path.dart
+++ lib/utils/file_path.dart
@@ -3,25 +3,14 @@ import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<File> getFilePath(String fileName) async {
-  File file = File('');
-
   if (Platform.isIOS) {
     final dir = await getApplicationDocumentsDirectory();
-    file = File('${dir.path}/$fileName');
+    return File('${dir.path}/$fileName');
   } else if (Platform.isAndroid) {
-    // Pedir permisos
-    var status = await Permission.manageExternalStorage.status;
-    if (!status.isGranted) {
-      status = await Permission.manageExternalStorage.request();
-    }
-
-    if (status.isGranted) {
-      final dir = await getExternalStorageDirectory();
-      if (dir != null) {
-        file = File('${dir.path}/$fileName');
-      }
-    }
+    final dir = await getExternalStorageDirectory(); // Asegura que el directorio existe
+    final path = '${dir?.path}/$fileName';
+    final file = File(path);
+    return file;
   }
-
-  return file;
+  throw UnsupportedError('Unsupported platform');
 }
